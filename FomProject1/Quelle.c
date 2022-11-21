bac0b6b8898abbb9009d71db36b83aae1ead1e4f
Jakob Reh <jakob.reh@gmx.de>
2022-11-21 15:26:55 +0100

    Arrays

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,7 +1,11 @@
 // 21.11.2022
+// Arrays
 #include <stdio.h>
+#define SIZE 14
 int main()
 {
-	//Falsche Ausgabe
-	printf("φόδί@'#<>|€");
+	float f[SIZE];
+	//Folgendes geht nicht, weil i dynamisch ist:
+	//int i = 14
+	//float z[i];
 };
