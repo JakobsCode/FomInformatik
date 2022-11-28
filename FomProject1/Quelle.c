c8c3a21309cb473f658f44328e39a444c7624bfe
Jakob Reh <jakob.reh@gmx.de>
2022-11-28 14:25:15 +0100

    return replace with break

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -11,7 +11,7 @@ void main()
 	time_t sec;						/*Aktuelle Zeit in Sekunden*/
 	int zufall = 0;					/*Zufallszahl initialisieren*/
 	int spielen = 1;				/*Entscheidet, ob erneut gespielt werden soll*/
-	while (spielen) {				/*Solange spielen == 1*/
+	while (spielen) {				/*Solange spielen == 1*/ 
 		sec = time(NULL);			/*Aktuelle Zeit abrufen*/
 		srand(sec);					/*Zufallsgenerator seeden (Anti-NSA)*/
 		zufall = rand() % 15 + 1;	/*Zufällige Zahl wird generiet*/
@@ -21,7 +21,7 @@ void main()
 			scanf_s("%d", &zahlgeraten);
 			if (zahlgeraten == zufall) {
 				printf("Richtige Zahl! Dies war dein %d|3 Versuch\n", i + 1);
-				return;
+				break;
 			}
 			else {
 				printf("Leider falsch, dass war dein %d|3 Versuch\n", i + 1);
