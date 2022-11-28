ae3188746c06ee37b18cc687aa6621ec626df793
Jakob Reh <jakob.reh@gmx.de>
2022-11-28 14:30:30 +0100

    Optimal hinzugefügt

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -11,7 +11,7 @@ void main()
 	time_t sec;						/*Aktuelle Zeit in Sekunden*/
 	int zufall = 0;					/*Zufallszahl initialisieren*/
 	int spielen = 1;				/*Entscheidet, ob erneut gespielt werden soll*/
-	while (spielen) {				/*Solange spielen == 1*/ 
+	while (spielen) {				/*Solange spielen == 1*/ //-->Optimal: Do-While
 		sec = time(NULL);			/*Aktuelle Zeit abrufen*/
 		srand(sec);					/*Zufallsgenerator seeden (Anti-NSA)*/
 		zufall = rand() % 15 + 1;	/*Zufällige Zahl wird generiet*/
@@ -19,7 +19,7 @@ void main()
 		for (int i = 0; i < 3; i++) {
 			printf("Deine Zahl: ");
 			scanf_s("%d", &zahlgeraten);
-			if (zahlgeraten == zufall) {
+			if (zahlgeraten == zufall) { //-->Optimal: Erst ungleich, wegen Wahrscheinlichkeiten
 				printf("Richtige Zahl! Dies war dein %d|3 Versuch\n", i + 1);
 				break;
 			}
