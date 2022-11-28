7d1560d5cd8b44e53a3f6bc8c5e97e11a8e01f62
Jakob Reh <jakob.reh@gmx.de>
2022-11-28 15:15:36 +0100

    Working part 1

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -4,38 +4,93 @@
 #include <stdlib.h>
 #include <time.h>
 #include "H1.h"
-
+//vabs
 void main()
 {
-	int zahlgeraten = 0;			/*Zahl der ratenen Person*/
-	time_t sec;						/*Aktuelle Zeit in Sekunden*/
-	int zufall = 0;					/*Zufallszahl initialisieren*/
-	int spielen = 1;				/*Entscheidet, ob erneut gespielt werden soll*/
-	while (spielen) {				/*Solange spielen == 1*/ //-->Optimal: Do-While
-		sec = time(NULL);			/*Aktuelle Zeit abrufen*/
-		srand(sec);					/*Zufallsgenerator seeden (Anti-NSA)*/
-		zufall = rand() % 15 + 1;	/*Zufällige Zahl wird generiet*/
-		printf("Zufallszahl wurde erstellt. Rate deine Zahl!\n");
-		for (int i = 0; i < 3; i++) {
-			printf("Deine Zahl: ");
-			scanf_s("%d", &zahlgeraten);
-			if (zahlgeraten == zufall) { //-->Optimal: Erst ungleich, wegen Wahrscheinlichkeiten
-				printf("Richtige Zahl! Dies war dein %d|3 Versuch\n", i + 1);
-				break;
-			}
-			else {
-				printf("Leider falsch, dass war dein %d|3 Versuch\n", i + 1);
-				if (zahlgeraten > zufall) {
-					printf("Die Zahl ist kleiner.\n");
-				}
-				else {
-					printf("Die Zahl ist groesser.\n");
-				}
-			}
-		}
-		printf("Die Zahl war: %d\n", zufall);
-		printf("Willst du nochmal spielen? NEIN=0, JA=1\n");
+	int keep_running = 1; // =true
+	int User_Data = 0; //Eingabe
+	int Zahl1 = 10;
+	int Zahl2 = 10;
+
+	do
+	{
+		printf("----------------------------------------------------------------------------\n");
+		printf("----------------------------------------------------------------------------\n");
+		printf("1 = Eingabe Zahl1\n");
+		printf("2 = Eingabe Zahl2\n");
+		printf("3 = Ausgabe Summe\n");
+		printf("4 = Ausgabe Produkt\n");
+		printf("5 = Ausgabe Subtraktion\n");
+		printf("6 = Ausgabe Division\n");
+		printf("7 = Ausgabe aller möglichen Multiplikationen von (1...Zahl1)*(1...Zahl2)\n");
+		printf("8 = Quatratwurtel für Zahl1 nach Heron-Verfahren\n");
+		printf("9 = Beenden\n");
+		printf("----------------------------------------------------------------------------\n");
+		printf("----------------------------------------------------------------------------\n");
+		printf("\n");
+		printf("\n");
+		printf("\n");
+
+
 		printf("Deine Eingabe: ");
-		scanf_s("%d", &spielen);
-	}
+		scanf_s("%d", &User_Data);
+		printf("\n");
+
+		switch(User_Data) {
+		case 1:
+			printf("Deine Eingabe für Zahl 1: ");
+			scanf_s("%d", &Zahl1);
+			printf("\n");
+			break;
+		case 2:
+			printf("Deine Eingabe für Zahl 2: ");
+			scanf_s("%d", &Zahl2);
+			printf("\n");
+			break;
+		case 3:
+			printf("Die Summe ist: %d", Zahl1 + Zahl1);
+			break;
+		case 4:
+			printf("Das Produkt ist: %d", Zahl1 * Zahl2);
+			break;
+		case 5:
+			printf("Die Differenz ist %d", Zahl1 - Zahl2);
+			break;
+		case 6:
+			if (Zahl2 != 0) {
+				printf("DIe Division ist %f", Zahl1 / Zahl2);
+			}
+			else
+			{
+				printf("Die Zahl 2 ist 0. Division nicht möglich!");
+			}
+			break;
+		case 7:
+			for (int i = 1; i < Zahl1; i++) {
+				for (int k = 1; k < Zahl2; k++) {
+					if (i * k < 10) {
+						printf("%d  ", i * k);
+					}
+					else
+					{
+						printf("%d ", i * k);
+					}
+				}
+				printf("\n");
+			}
+			break;
+		case 8:
+
+			break;
+		case 9:
+			printf("Bis zum naechsten mal!");
+			keep_running = 0;
+			break;
+		default:
+			printf("Lesen muss gekoennt sein....");
+			break;
+		}
+
+
+	} while (keep_running);
 }
