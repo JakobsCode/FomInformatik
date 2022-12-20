20546266b1a42d5446e2144d0d1a6a48cf2789ea
Jakob Reh <jakob.reh@gmx.de>
2022-12-20 15:09:02 +0100

    ggT Rekusion

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,41 +1,17 @@
-// 28.11.2022
-//https://github.com/JakobsCode/FomInformatik
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <math.h>
+#include <ctype.h>
 #include "H1.h"
+#include <time.h>
 
-void main()
-{
-	int zahlgeraten = 0;			/*Zahl der ratenen Person*/
-	time_t sec;						/*Aktuelle Zeit in Sekunden*/
-	int zufall = 0;					/*Zufallszahl initialisieren*/
-	int spielen = 1;				/*Entscheidet, ob erneut gespielt werden soll*/
-	while (spielen) {				/*Solange spielen == 1*/
-		sec = time(NULL);			/*Aktuelle Zeit abrufen*/
-		srand(sec);					/*Zufallsgenerator seeden (Anti-NSA)*/
-		zufall = rand() % 15 + 1;	/*Zufällige Zahl wird generiet*/
-		printf("Zufallszahl wurde erstellt. Rate deine Zahl!\n");
-		for (int i = 0; i < 3; i++) {
-			printf("Deine Zahl: ");
-			scanf_s("%d", &zahlgeraten);
-			if (zahlgeraten == zufall) {
-				printf("Richtige Zahl! Dies war dein %d|3 Versuch\n", i + 1);
-				return;
-			}
-			else {
-				printf("Leider falsch, dass war dein %d|3 Versuch\n", i + 1);
-				if (zahlgeraten > zufall) {
-					printf("Die Zahl ist kleiner.\n");
-				}
-				else {
-					printf("Die Zahl ist groesser.\n");
-				}
-			}
-		}
-		printf("Die Zahl war: %d\n", zufall);
-		printf("Willst du nochmal spielen? NEIN=0, JA=1\n");
-		printf("Deine Eingabe: ");
-		scanf_s("%d", &spielen);
-	}
+unsigned int ggT(unsigned int a, unsigned int b) {
+	if (b == 0) return a;
+	return ggT(b, a % b);
+}
+
+
+int main() {
+	printf("%d",ggT(123456, 131312));
+	return 0;
 }
