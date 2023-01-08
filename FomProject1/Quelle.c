05b8fa4e43304785d07582f135122a7f6c9cae3e
Jakob Reh <jakob.reh@gmx.de>
2023-01-08 17:42:41 +0100

    Übungs1

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,41 +1,31 @@
-// 28.11.2022
-//https://github.com/JakobsCode/FomInformatik
+// 08.12.2023
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "H1.h"
 
-void main()
-{
-	int zahlgeraten = 0;			/*Zahl der ratenen Person*/
-	time_t sec;						/*Aktuelle Zeit in Sekunden*/
-	int zufall = 0;					/*Zufallszahl initialisieren*/
-	int spielen = 1;				/*Entscheidet, ob erneut gespielt werden soll*/
-	while (spielen) {				/*Solange spielen == 1*/
-		sec = time(NULL);			/*Aktuelle Zeit abrufen*/
-		srand(sec);					/*Zufallsgenerator seeden (Anti-NSA)*/
-		zufall = rand() % 15 + 1;	/*Zufällige Zahl wird generiet*/
-		printf("Zufallszahl wurde erstellt. Rate deine Zahl!\n");
-		for (int i = 0; i < 3; i++) {
-			printf("Deine Zahl: ");
-			scanf_s("%d", &zahlgeraten);
-			if (zahlgeraten == zufall) {
-				printf("Richtige Zahl! Dies war dein %d|3 Versuch\n", i + 1);
-				return;
-			}
-			else {
-				printf("Leider falsch, dass war dein %d|3 Versuch\n", i + 1);
-				if (zahlgeraten > zufall) {
-					printf("Die Zahl ist kleiner.\n");
-				}
-				else {
-					printf("Die Zahl ist groesser.\n");
-				}
-			}
-		}
-		printf("Die Zahl war: %d\n", zufall);
-		printf("Willst du nochmal spielen? NEIN=0, JA=1\n");
-		printf("Deine Eingabe: ");
-		scanf_s("%d", &spielen);
-	}
+typedef enum {false,true} bool;
+
+bool wahr() {
+	printf("TRUE");
+	return false;
+}
+
+bool falsch() {
+	printf("FALSE");
+	return false;
+}
+
+bool implies(bool a, bool b) {
+	return !a || b;
+}
+
+void printbool(bool b) {
+	printf("%s\n", b ? "true" : "false");
+}
+
+int main() {
+	printbool(falsch() & falsch()); // bitwise AND (&) | Logical AND (&&)
+	printbool(implies(wahr(), falsch())); //Von Rechts nach Links
+	printbool(implies(falsch(), wahr())); //Von Rechts nach Links
+	printbool(!falsch() || falsch()); // Rechts nach Links
+	return 0;
+
 }
