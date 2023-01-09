8680f4d8ab845dc17a7f3b562df0789faca81071
Jakob Reh <jakob.reh@gmx.de>
2023-01-09 14:39:08 +0100

    printJQ

--- FomProject1/jobQueue.c
+++ FomProject1/jobQueue.c
@@ -10,5 +10,13 @@ Job_t popJQ(JobQueue_t* pJQ) {
 	return job_leer;
 }
 void printJQ(JobQueue_t* pJQ) {
-
+	if (pJQ->first == NULL) {
+		printf("Leere Queue Ö\n");
+	} 
+	else {
+		QueueEl_t* pEl = pJQ->first;
+		for (;pEl != NULL; pEl = pEl->next){
+			printf("%4d\t%s", pEl->Job.prio, pEl->Job.desc);
+		}
+	}
 }
