c5a7e26117536d89a8ffaa3850964eb585ff4f94
Jakob Reh <jakob.reh@gmx.de>
2023-01-09 12:41:03 +0100

    pre1

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-void heron(double x, double* result) {
-    // Anfangswert für die Iteration festlegen
-    double last = x;
-    double next = (last + x / last) / 2.0;
+int main() {
 
-    // Iteration solange fortsetzen, bis sich der Wert kaum noch verändert
-    while (fabs(last - next) > 1e-9) {
-        last = next;
-        next = (last + x / last) / 2.0;
-    }
-
-    // Ergebnis zurückgeben
-    *result = next;
-}
-
-int main(void) {
-    double x = 9.0;
-    double result;
-    heron(x, &result);
-    printf("Die Quadratwurzel von %f ist %f\n", x, result);
-    return 0;
+	return 0;
 }
