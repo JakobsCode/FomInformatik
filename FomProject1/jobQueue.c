582fae963efdb39dea54971f7e498d490780ba30
Jakob Reh <jakob.reh@gmx.de>
2023-01-09 13:43:22 +0100

    schritt1

---
+++ FomProject1/jobQueue.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "jobQueue.h"
+
+int pushJQ(JobQueue_t* pJQ, Job_t* pJob) {
+	return 0;
+}
+Job_t popJQ(JobQueue_t* pJQ) {
+	Job_t job_leer = { 0 };
+	return job_leer;
+}
+void printJQ(JobQueue_t* pJQ) {
+
+}
