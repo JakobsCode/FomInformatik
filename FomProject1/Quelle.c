be6a91408ff3d224a060a630dfcbe570b8be19f8
Jakob Reh <jakob.reh@gmx.de>
2023-01-09 12:31:22 +0100

    hmm

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <ctype.h>
-#include "H1.h"
-#include <time.h>
 
-unsigned int ggT(unsigned int a, unsigned int b) {
-	if (b == 0) return a;
-	return ggT(b, a % b);
+void heron(double x, double* result) {
+    // Anfangswert für die Iteration festlegen
+    double last = x;
+    double next = (last + x / last) / 2.0;
+
+    // Iteration solange fortsetzen, bis sich der Wert kaum noch verändert
+    while (fabs(last - next) > 1e-9) {
+        last = next;
+        next = (last + x / last) / 2.0;
+    }
+
+    // Ergebnis zurückgeben
+    *result = next;
 }
 
-
-int main() {
-	printf("%d",ggT(123456, 131312));
-	return 0;
+int main(void) {
+    double x = 9.0;
+    double result;
+    heron(x, &result);
+    printf("Die Quadratwurzel von %f ist %f\n", x, result);
+    return 0;
 }
