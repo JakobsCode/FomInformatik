c495e7a279224f1a5b42db601de1358fe052ceb4
Jakob Reh <jakob.reh@gmx.de>
2023-01-17 15:09:03 +0100

    Endlich Geschafft :D

--- FomProject1/Main.c
+++ FomProject1/Main.c
@@ -2,29 +2,37 @@
 #include "jobQueue.h"
 
 int main() {
-	JobQueue_t jobQueue_1, jobQueue_2 = { 0 };
-	Job_t	Job_1 = {50, "Urlaubsplanung"},
-			Job_2 = {5, "Zähne putzen"},
-			Job_3 = {15, "Früh aufstehen"},
-			Job_4 = {34, "E-Mails beantworten"},
-			Job_5 = { 0 };
+	JobQueue_t jobQueue_1 = {0}, jobQueue_2 = {0};
+	Job_t	Job_1 = { 50, "Urlaubsplanung" },
+		Job_2 = { 5, "Zähne putzen" },
+		Job_3 = { 15, "Früh aufstehen" },
+		Job_4 = { 34, "E-Mails beantworten" },
+		Job_5 = { 10, "Super Klaus" },
+		Job_6 = { 9, "Agile Working" },
+		Job_7 = { 99, "FOM Technik"};
 	
 
 	pushJQ(&jobQueue_1, &Job_1);
 	pushJQ(&jobQueue_1, &Job_2);
 	pushJQ(&jobQueue_1, &Job_3);
+	pushJQ(&jobQueue_1, &Job_4);
+	pushJQ(&jobQueue_1, &Job_5);
+	pushJQ(&jobQueue_1, &Job_6);
+	pushJQ(&jobQueue_1, &Job_7);
 
 	printJQ(&jobQueue_1);
-
+	printf("Ende Queue 1\n");
 	pushJQ(&jobQueue_2, &Job_4);
 	pushJQ(&jobQueue_2, &Job_3);
 
 	Job_5 = popJQ(&jobQueue_1);
 
 	printJQ(&jobQueue_1);
+	printf("Ende Queue 1\n");
 	printJQ(&jobQueue_2);
+	printf("Ende Queue 2\n");
 
-	printf("%d",jobQueue_1.count);
+	printf("Job Queue 1 Count: %d\n",jobQueue_1.count);
 
 	return 0;
 }
--- FomProject1/jobQueue.c
+++ FomProject1/jobQueue.c
@@ -3,22 +3,54 @@
 #include "jobQueue.h"
 
 int pushJQ(JobQueue_t* pJQ, Job_t* pJob) {
-	return 0;
+	QueueEl_t* pNewEl = calloc(1, sizeof(QueueEl_t)); //=> pNewEl->next = NULL
+	if (pNewEl == NULL) return 0;
+
+	pNewEl->Job = *pJob;
+	if (++pJQ->count == 1) pJQ->first = pJQ->last = pNewEl;
+	else {
+		QueueEl_t* pEl = pJQ->first, * pPrevEl = NULL;
+		for (; pPrevEl != pJQ->last && pEl->Job.prio > pNewEl->Job.prio; pEl = pEl->next) pPrevEl = pEl;
+		if (pPrevEl == NULL) {
+			pNewEl->next = pJQ->first;
+			pJQ->first = pNewEl;
+		}
+		else
+		{
+			pNewEl->next = pPrevEl->next;
+			pPrevEl->next = pNewEl;
+			if (pPrevEl == pJQ->last) pJQ->last = pNewEl;
+		}
+
+	}
+	return 1;
 }
 
 Job_t popJQ(JobQueue_t* pJQ) {
-	Job_t job_leer = { 0 };
-	return job_leer;
+	Job_t job_temp = { 0 };
+	if (pJQ->first == NULL) printf("Queue ist leer\n");
+	else
+	{
+		QueueEl_t* QueueEl = pJQ->first;
+
+		job_temp = QueueEl->Job;
+
+		if (--pJQ->count == 0) pJQ->first = pJQ->last = NULL;
+		else pJQ->first = pJQ->first->next;
+
+		free(QueueEl);
+	}
+	return job_temp;
 }
 
 void printJQ(JobQueue_t* pJQ) { // FERTIG
 	if (pJQ->first == NULL) {
 		printf("Leere Queue (Ö)\n");
-	} 
+	}
 	else {
 		QueueEl_t* pEl = pJQ->first;
-		for (;pEl != NULL; pEl = pEl->next){
-			printf("%4d\t%s", pEl->Job.prio, pEl->Job.desc);
+		for (; pEl != NULL; pEl = pEl->next) {
+			printf("%4d  %s\n", pEl->Job.prio, pEl->Job.desc);
 		}
 	}
 }
