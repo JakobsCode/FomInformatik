f0eb71f1d27eccc6d18759cd988947b93aef7b73
Jakob Reh <jakob.reh@gmx.de>
2023-01-26 20:49:29 +0100

    Fertig

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int a = 10;		// Res. Adr. für 1x INT		| Schreiben "10" in Adr.			| ADRESSE ALS INT INTERP.
+	int* p = &a;	// Res. Adr. für 1x POINTER | Schreibe [ADRESSE VON A] in Adr.	| ADRESSE ALS POINTER INTERP.
+	int** pp = &p;	// Res. Adr. für 1x POINTER | Schreibe [ADRESSE VON P] in Adr.	| ADRESSE ALS DOPPEL POINTER INTERP.
 
-int symetricArray(char* array, int firstIndex, int secondIndex)
-{
-	if (array[firstIndex] != array[secondIndex]) return 0;
-	if (secondIndex - firstIndex <= 1) return 1;
-	return symetricArray(array, firstIndex + 1, secondIndex - 1);
-}
-
-
-int main()
-{
-	char myArray[] = "OtZTKtO";
-	printf("%d\n", symmetricArrayIndex(myArray, 0, 6));
+	printf("a\t= %d\n", a);
+	printf("*p\t= %d\n", *p);
+	printf("**pp\t= %d\n", **pp);
+	printf("\n");
+	printf("&a\t= %d\n", &a);
+	printf("*pp\t= %d\n", *pp);
+	printf("p\t= %d\n", p);
+	printf("\n");
+	printf("&p\t= %d\n", &p);
+	printf("pp\t= %d\n", pp);
+	printf("\n");
+	printf("&pp\t= %d\n", &pp);
+	return 0;
 }
