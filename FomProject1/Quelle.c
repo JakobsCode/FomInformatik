4fc354661f90a1dac0856809b8f8812fd9f5a8c4
Jakob Reh <jakob.reh@gmx.de>
2023-01-26 20:56:13 +0100

    hex

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -9,14 +9,14 @@ int main() {
 	printf("*p\t= %d\n", *p);
 	printf("**pp\t= %d\n", **pp);
 	printf("\n");
-	printf("&a\t= %d\n", &a);
-	printf("*pp\t= %d\n", *pp);
-	printf("p\t= %d\n", p);
+	printf("&a\t= %p\n", &a);
+	printf("*pp\t= %p\n", *pp);
+	printf("p\t= %p\n", p);
 	printf("\n");
-	printf("&p\t= %d\n", &p);
-	printf("pp\t= %d\n", pp);
+	printf("&p\t= %p\n", &p);
+	printf("pp\t= %p\n", pp);
 	printf("\n");
-	printf("&pp\t= %d\n", &pp);
+	printf("&pp\t= %p\n", &pp);
 	return 0;
 }
 
@@ -27,13 +27,13 @@ a       = 10
 *p      = 10
 **pp    = 10
 
-&a      = 13892372
-*pp     = 13892372
-p       = 13892372
+&a      = 0097FCA4
+*pp     = 0097FCA4
+p       = 0097FCA4
 
-&p      = 13892360
-pp      = 13892360
+&p      = 0097FC98
+pp      = 0097FC98
 
-&pp     = 13892348
+&pp     = 0097FC8C
 
 */
