c3dccf9d57787c6dad80d3bc32ddec251230e73e
Jakob Reh <jakob.reh@gmx.de>
2023-01-26 21:00:28 +0100

    frech

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 int main() {
 	int a = 10;		// Res. Adr. für 1x INT		| Schreiben "10" in Adr.			| ADRESSE ALS INT INTERP.
-	int* p = &a;	// Res. Adr. für 1x POINTER | Schreibe [ADRESSE VON A] in Adr.	| ADRESSE ALS POINTER INTERP.
+	int p = &a;	// Res. Adr. für 1x POINTER | Schreibe [ADRESSE VON A] in Adr.	| ADRESSE ALS POINTER INTERP.
 	int** pp = &p;	// Res. Adr. für 1x POINTER | Schreibe [ADRESSE VON P] in Adr.	| ADRESSE ALS DOPPEL POINTER INTERP.
 
 	printf("a\t= %d\n", a);
-	printf("*p\t= %d\n", *p);
+	printf("*p\t= %d\n", *(int*)p);
 	printf("**pp\t= %d\n", **pp);
 	printf("\n");
 	printf("&a\t= %p\n", &a);
 	printf("*pp\t= %p\n", *pp);
-	printf("p\t= %p\n", p);
+	printf("p\t= %p\n", (int*)p);
 	printf("\n");
-	printf("&p\t= %p\n", &p);
+	printf("&p\t= %p\n", &(int*)p);
 	printf("pp\t= %p\n", pp);
 	printf("\n");
 	printf("&pp\t= %p\n", &pp);
