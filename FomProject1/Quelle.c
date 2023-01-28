35323886721a6c88f4319b2a330b637b515068f5
Jakob Reh <jakob.reh@gmx.de>
2023-01-28 12:29:27 +0100

    ben

--- FomProject1/Quelle.c
+++ FomProject1/Quelle.c
@@ -1,41 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
-#define rows 10
-#define cols 10
 
-void createMatrix(int matrix[rows][cols])
-{
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			if ((i % 2 != 0) && (j % 2 != 0))
-			{
-				matrix[i][j] = (-1) * i * j;
-			}
-			else
-			{
-				matrix[i][j] = i * j;
-			}
-		}
+int main() {
+	int** pparray = (int**)calloc(5, sizeof(int*));
+
+	for (int i = 0; i < 5; i++) {
+		*(pparray + i) = (int*)calloc(7, sizeof(int));
 	}
-}
 
 
-int main()
-{
-	int myMatrix[rows][cols]; // <---DOPPELPOINTER | array[][] == **array 
-	createMatrix(myMatrix);
-	int negCount = 0;
-	for (int i = 0; i < (rows * cols); i++)
-	{
-		printf("%d\n", *(	(int*)myMatrix	 +	i));
-	  //printf("%d\n", *(	myMatrix		 +  i)); <-- Falsch
 
-		if (*(((int*)myMatrix) + i) < 0)
-		{
-			negCount++;
-		}
+
+	for (int i = 0; i < 5; i++) {
+		free(*(pparray + i));
 	}
-	printf("negCount = %d\n", negCount);
-	return 0;
+	free(pparray);
+
 }
